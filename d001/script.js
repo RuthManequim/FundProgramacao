50648161a94c393b62f4bc753f793563685e82a4
Ruth Rodrigues <154280843+RuthManequim@users.noreply.github.com>
2023-12-20 17:04:19 -0300

    Add files via upload

---
+++ d001/script.js
@@ -0,0 +1,9 @@
+function clicar1(){
+    alert("Você clicou no botão 1");
+}
+function clicar2(){
+    alert("Você clicou no botão 2");
+}
+function clicar3(){
+    alert("Você clicou no botão 3");
+}
